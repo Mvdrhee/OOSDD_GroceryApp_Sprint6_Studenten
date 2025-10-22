2afcc3f87a18c41cf78449cdcbe54db8bc5ea8c9
Michael <s1197491@student.windesheim.nl>
2025-10-22 11:52:21 +0200

    Gebruikers die geen admin zijn kunnen geen producten toevoegen

--- Grocery.App/ViewModels/NewProductViewModel.cs
+++ Grocery.App/ViewModels/NewProductViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
@@ -9,16 +10,20 @@ namespace Grocery.App.ViewModels
     {
         public ObservableCollection<Product> Products { get; set; }
         private readonly IProductService _productsService;
-        public NewProductViewModel(IProductService productService)
+        [ObservableProperty]
+        Client client;
+        public NewProductViewModel(IProductService productService, GlobalViewModel global)
         {
             Title = "Producten toevoegen";
             _productsService = productService;
             Products = new(productService.GetAll());
+            client = global.Client;
         }
 
         [RelayCommand]
         private void AddProduct(string name, int stock, DateOnly date, Decimal price)
         {
+            if (Client.Role != Role.Admin) {return; }
             Product new_product = new(0, name, stock, date, price);
             _productsService.Add(new_product);
             Products.Add(new_product);
