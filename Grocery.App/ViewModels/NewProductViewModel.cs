fcee3f96412d2222166e10eb397f6dc24903ec5b
Michael <s1197491@student.windesheim.nl>
2025-10-22 11:49:58 +0200

    AddProductViewModel toevoegeb

---
+++ Grocery.App/ViewModels/NewProductViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+
+namespace Grocery.App.ViewModels
+{
+    public partial class NewProductViewModel : BaseViewModel
+    {
+        public ObservableCollection<Product> Products { get; set; }
+        private readonly IProductService _productsService;
+        public NewProductViewModel(IProductService productService)
+        {
+            Title = "Producten toevoegen";
+            _productsService = productService;
+            Products = new(productService.GetAll());
+        }
+
+        [RelayCommand]
+        private void AddProduct(string name, int stock, DateOnly date, Decimal price)
+        {
+            Product new_product = new(0, name, stock, date, price);
+            _productsService.Add(new_product);
+            Products.Add(new_product);
+
+        }
+    }
+}
