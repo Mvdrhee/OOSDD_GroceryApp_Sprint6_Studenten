4836d866a0bf9575fdc0a9d4d0837432d90692f2
Michael <s1197491@student.windesheim.nl>
2025-10-24 11:19:28 +0200

    Eerste implementatie

--- Grocery.App/ViewModels/NewProductViewModel.cs
+++ Grocery.App/ViewModels/NewProductViewModel.cs
@@ -10,8 +10,25 @@ namespace Grocery.App.ViewModels
     {
         public ObservableCollection<Product> Products { get; set; }
         private readonly IProductService _productsService;
+
         [ObservableProperty]
-        Client client;
+        private Client client;
+
+        [ObservableProperty]
+        private string inputName = "";
+
+        [ObservableProperty]
+        private string inputStock = "";
+
+        [ObservableProperty]
+        private string inputPrice = "";
+
+        [ObservableProperty]
+        private string inputDate = "";
+
+        [ObservableProperty]
+        private string errorMessage = "";
+
         public NewProductViewModel(IProductService productService, GlobalViewModel global)
         {
             Title = "Producten toevoegen";
@@ -21,13 +38,21 @@ namespace Grocery.App.ViewModels
         }
 
         [RelayCommand]
-        private void AddProduct(string name, int stock, DateOnly date, Decimal price)
+        private void AddProduct()
         {
             if (Client.Role != Role.Admin) {return; }
-            Product new_product = new(0, name, stock, date, price);
+            Product? new_product = null;
+            try
+                { new_product = new(0, InputName, Int32.Parse(InputStock), DateOnly.Parse(InputDate), Int32.Parse(InputPrice)); }
+            catch (Exception)
+            {
+                ErrorMessage = "Ongeldige invoer";
+                return;
+            }
+
             _productsService.Add(new_product);
             Products.Add(new_product);
 
-        }
+            }
     }
 }
